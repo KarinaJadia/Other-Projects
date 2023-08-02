38fdd68169d40a452d135db55afec0652bcaa470
Karina2638 <karina.jadia@gmail.com>
2023-08-02 17:45:44 -0400

    base done, maybe i'll add some pretty things now

--- speed-accuracy-typer.py
+++ speed-accuracy-typer.py
@@ -13,7 +13,7 @@ def print_pretty(list, row = None): # prints the matrix in a pretty way, mostly
                 print(list[j][i], end=' ')
             print('\n')
 
-rows = 5 # row 1 - word, row 2 - length, row 3 - incorrect letters
+rows = 4 # row 1 - word, row 2 - length, row 3 - incorrect letters
 columns = 20 # 20 words
 words = [[0 for x in range(columns)] for y in range(rows)] # initializes matrix
 '''     initializes as: 
@@ -22,8 +22,7 @@ words = [[0 for x in range(columns)] for y in range(rows)] # initializes matrix
         0  [ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],      tracks the word
         1    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],      tracks length of word
         2    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],      tracks user's input
-        3    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],      tracks user's incorrect letters
-        4    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ]     tracks time for each word
+        3    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ]     tracks time for each word
 '''
 
 # initializes the words to type
@@ -51,19 +50,37 @@ words[0][19] = 'dropping'
 print("I'm going to time how long it takes for you to type a bunch of words and also test your accuracy. Hit enter to start. Good luck!")
 enter = input()
 
-for i in range(len(words[0])):
+for i in range(len(words[0])): # the test part of the program
     print(words[0][i])
     s = time.time() # times user
     words[2][i] = input() # updates matrix with user's input
     e = time.time()
-    words[4][i] = e-s # puts time for each word in matrix
+    words[3][i] = e-s # puts time for each word in matrix
     print('\n')
 
-total = 0
-for i in words[4]:
+total = 0 # will store the total time taken to type each word
+for i in words[3]: # adds up the total time taken to add each word
     total += i
 
+wpm = len(words[0])/((total)/60) # calculates the words per minute
+
+total_letters = 0
+for i in range(len(words[0])):
+    total_letters += len(words[0][i])
+
+total_mistakes = 0
+for i in range(len(words[0])): # counts a mistake if the user doesn't enter a letter or if they enter the wrong letter
+    if not len(words[0][i]) == len(words[2][i]): # tests if the user didn't put the right amount of letters
+        total_mistakes += abs((len(words[0][i])) - (len(words[2][i]))) # adds up missing letters
+    else: # tests if the user entered the wrong letter
+        for letter in words[0][i]:
+            user = words[2][i]
+            if letter not in user:
+                total_mistakes += 1
+
+accuracy = (total_letters-total_mistakes)/total_letters * 100
+
 input(f'That took {total:.2f} seconds!\n\nhit enter to see each stat')
-input(f'- words per minute: {len(words[0])/((total)/60):.2f}') # calculates words per minute
-input(f'- accuracy: ')
+input(f'- words per minute: {wpm:.2f}')
+input(f'- accuracy: {accuracy:.2f}%')
 print('\n')
