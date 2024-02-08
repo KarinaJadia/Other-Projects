7d2c914ee4f5132b35cbeb9da694943ad6f2ad2f
KarinaJadia <karina.jadia@gmail.com>
2024-02-08 17:47:48 -0500

    completed, needs to be dusted

--- matrix-solver.py
+++ matrix-solver.py
@@ -6,7 +6,7 @@ print("also i do not have any try/catch statements so if you don't follow the in
 def print_matrix(m): # makes printing the matrix prettier
     for r in range(len(m)):
         for c in range(len(m[0])):
-            print(f'{m[r][c]}', end=' ')
+            print(f'{m[r][c]:.1f}', end=' ')
         print('')
     print('')
 
@@ -38,7 +38,22 @@ print_matrix(m)
 to_do = 0 # this keeps track of which column to go to
 level = 0 # this keeps track of which row we're on for pivots
 
-while to_do < c:
+while to_do < c and level < r:
     pivot = m[level][to_do]
     col = list_yank(m, to_do)
+    for i, e in enumerate(col):
+        if i == level:
+            pass
+        else:
+            multiple = -e/pivot
+            print('pivot ', pivot)
+            print('multiple ',multiple)
+            x = m[i]
+            print('row to convert ',x)
+            m[i] = [m[level][z] * multiple + x[z] for z in range(c)]
+            print(f'final row done: {m[i]}')
+    
+    print('')
+    print_matrix(m)
     to_do += 1
+    level += 1
