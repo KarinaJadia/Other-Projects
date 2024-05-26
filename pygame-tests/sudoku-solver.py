cc62f0589b0b92a24040eb4eb182deb2ece0a1cb
KarinaJadia <karina.jadia@gmail.com>
2024-05-26 18:27:27 -0400

    started solve function...

--- pygame-tests/sudoku-solver.py
+++ pygame-tests/sudoku-solver.py
@@ -21,7 +21,7 @@ pos = -20
 colors = [[(105, 64, 70), (105, 80, 64), (105, 93, 64)], # stores the colors of the box
           [(64, 105, 89), (64, 102, 105), (64, 81, 105)],
           [(64, 64, 105), (87, 64, 105), (98, 64, 105)]]
-set_num = [[1, 2, 3], # stores which larger set it is in
+set_num = [[1, 2, 3], # to make life easier
           [4, 5, 6],
           [7, 8, 9]]
 
@@ -37,9 +37,15 @@ for i in range(81):
         "active": False,
         "row": i // 9 + 1, # stores row (goes from 1 to 9)
         "col": i % 9 + 1, # stores column (goes from 1 to 9)
-        "set": set_num[(i//9)//3][(i%9)//3]
+        "set": set_num[(i//9)//3][(i%9)//3] # which 'set' of boxes it is (goes from 1 to 9)
     }
     boxes.append(box)
+
+def solve():
+    for box in boxes:
+        if box['text'] == '':
+            box['passive color'] = 'red'
+            box['color'] = 'red'
 
 # game
 while True:
@@ -65,8 +71,7 @@ while True:
 
             # if enter key is clicked
             if event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN:
-                pygame.quit()
-                sys.exit()
+                solve()
 
             # if backspace is clicked
             elif event.key == pygame.K_BACKSPACE:
