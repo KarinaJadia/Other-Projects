ca6abf428f5e0f0d7c9f328f625bac07b1fd811e
KarinaJadia <karina.jadia@gmail.com>
2024-05-27 18:31:18 -0400

    HAVING TO MANUALLY DO POINTERS ASBASJKDFSAJ;L

--- pygame-tests/sudoku-solver.py
+++ pygame-tests/sudoku-solver.py
@@ -32,78 +32,11 @@ for i in range(81):
     boxes.append(box(80 + i % 9 * SPACE, pos, colors[(i//9)//3][(i%9)//3], colors[(i//9)//3][(i%9)//3],set_num[(i//9)//3][(i%9)//3]*100 + (i//9+1) * 10 + i%9+1, i))
 
 def solve():
-
-    set1 = {'possible': [1,2,3,4,5,6,7,8,9],'unsolved': []}
-    set2 = {'possible': [1,2,3,4,5,6,7,8,9],'unsolved': []}
-    set3 = {'possible': [1,2,3,4,5,6,7,8,9],'unsolved': []}
-    set4 = {'possible': [1,2,3,4,5,6,7,8,9],'unsolved': []}
-    set5 = {'possible': [1,2,3,4,5,6,7,8,9],'unsolved': []}
-    set6 = {'possible': [1,2,3,4,5,6,7,8,9],'unsolved': []}
-    set7 = {'possible': [1,2,3,4,5,6,7,8,9],'unsolved': []}
-    set8 = {'possible': [1,2,3,4,5,6,7,8,9],'unsolved': []}
-    set9 = {'possible': [1,2,3,4,5,6,7,8,9],'unsolved': []}
-
     for box in boxes:
-        # fancy color changing
-        if box[box['id']] == '':
-            box['passive color'] = (252, 186, 3)
-            box['color'] = (252, 186, 3)
-
-        # each set dictionary will store the leftover possible solutions within the set and the unsolved boxes
-        if box['id'] < 200:
-            if box[box['id']] != '':
-                set1['possible'].remove(int(box[box['id']]))
-            else:
-                set1['unsolved'].append(box['id'])
-                set1[box['pos']] = 0
-        elif box['id'] < 300:
-            if box[box['id']] != '':
-                set2[box['id']] = int(box[box['id']])
-                set2['possible'].remove(int(box[box['id']]))
-            else:
-                set2['unsolved'].append(box['id'])
-        elif box['id'] < 400:
-            if box[box['id']] != '':
-                set3[box['id']] = int(box[box['id']])
-                set3['possible'].remove(int(box[box['id']]))
-            else:
-                set3['unsolved'].append(box['id'])
-        elif box['id'] < 500:
-            if box[box['id']] != '':
-                set4[box['id']] = int(box[box['id']])
-                set4['possible'].remove(int(box[box['id']]))
-            else:
-                set4['unsolved'].append(box['id'])
-        elif box['id'] < 600:
-            if box[box['id']] != '':
-                set5[box['id']] = int(box[box['id']])
-                set5['possible'].remove(int(box[box['id']]))
-            else:
-                set5['unsolved'].append(box['id'])
-        elif box['id'] < 700:
-            if box[box['id']] != '':
-                set6[box['id']] = int(box[box['id']])
-                set6['possible'].remove(int(box[box['id']]))
-            else:
-                set6['unsolved'].append(box['id'])
-        elif box['id'] < 800:
-            if box[box['id']] != '':
-                set7[box['id']] = int(box[box['id']])
-                set7['possible'].remove(int(box[box['id']]))
-            else:
-                set7['unsolved'].append(box['id'])
-        elif box['id'] < 900:
-            if box[box['id']] != '':
-                set8[box['id']] = int(box[box['id']])
-                set8['possible'].remove(int(box[box['id']]))
-            else:
-                set8['unsolved'].append(box['id'])
-        else:
-            if box[box['id']] != '':
-                set9[box['id']] = int(box[box['id']])
-                set9['possible'].remove(int(box[box['id']]))
-            else:
-                set9['unsolved'].append(box['id'])
+        if box.text == '':
+            box.text = str(box.id)
+            box.passive_color = (252, 186, 3)
+            box.color = (252, 186, 3)
 
 # game
 while True:
@@ -129,8 +62,7 @@ while True:
 
             # if enter key is clicked
             if event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN:
-                #solve()
-                print(boxes[0].x)
+                solve()
     
             # if backspace is clicked
             elif event.key == pygame.K_BACKSPACE:
