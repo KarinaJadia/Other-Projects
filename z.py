ecb3c8e1d1a0260ab773a914d8604db0d73db788
KarinaJadia <karina.jadia@gmail.com>
2024-10-29 01:20:01 -0400

    testing somethig

--- z.py
+++ z.py
@@ -1,4 +1,8 @@
-side1 = input('side one: ')
-side2 = input('side two: ')
-side3 = input('side three: ')
-side4 = input('side four: ')
+# given a cash value and number of coins, solves how many of what coins
+
+# example: if i have 46¢ and five coins, how many dimes do i have?
+
+coins = {'p': 0, 'n': 0, 'd': 0, 'q':0}
+num_coins = int(input('how many coins do you have: '))
+amount = int(input('how many cents is it: '))
+
