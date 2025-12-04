f042555619129b3ff08cd4128b8bc0a002a1d344
KarinaJadia <karina.jadia@gmail.com>
2025-12-04 10:22:30 -0500

    :construction: experiment, ignore

---
+++ Main.java
@@ -0,0 +1,8 @@
+// to use, do javac Main then java Main
+// or hit the play button on the top right
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
