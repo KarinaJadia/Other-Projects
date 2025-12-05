c14bfd145e8bfb1700a1326d4717c3a1e83c5849
KarinaJadia <karina.jadia@gmail.com>
2025-12-04 20:08:52 -0500

    :construction: lowkey what if i quit

--- chem_equation_balancer.py
+++ chem_equation_balancer.py
@@ -1,6 +1,29 @@
 '''
 example inputs/outputs:
-_P4 + _O2 = _P2O5 -> 1, 5, 2
-_MgF2 + _Li2CO3 = _MgCO3 + _LiF -> 1, 1, 1, 2
-_Al + _H2SO4 = _Al2(SO4)3 + _H3 -> 2, 3, 1, 3
+P4 + O2 = P2O5 -> 1, 5, 2
+MgF2 + Li2CO3 = MgCO3 + LiF -> 1, 1, 1, 2
+Al + H2SO4 = Al2(SO4)3 + H3 -> 2, 3, 1, 3
 '''
+
+def splitter(eq):
+    ''' splits the equation into left and right '''
+    d = {'left': [], 'right': []}
+    eq = eq.split()
+
+    flag = 'left'
+    for i in eq:
+        if i == '=':
+            flag = 'right'
+        elif i == '+':
+            pass
+        elif flag == 'left':
+            d['left'].append(i)
+        else:
+            d['right'].append(i)
+
+    return d
+
+if __name__ == "__main__":
+    inp = input('enter equation: ')
+    parsed = splitter(inp)
+    print(parsed)
