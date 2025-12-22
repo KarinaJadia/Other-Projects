8f7a8fc1f8c54e822884d8614712e65f821e7d9e
KarinaJadia <karina.jadia@gmail.com>
2025-12-22 18:28:10 -0500

    :poop: i've got like art block for coding

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
-// to use, do javac Main then java Main
-// or hit the play button on the top right
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
     System.out.println("Hello World");
+    String response = input.nextLine();
+    System.out.println(response);
+    input.close();
   }
 }
