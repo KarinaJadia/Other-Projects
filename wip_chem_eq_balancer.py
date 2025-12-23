73189f368279ebd90aaa405dadc40c5d5c9369ea
KarinaJadia <karina.jadia@gmail.com>
2025-12-23 17:41:05 -0500

    💥 atom splitter function

--- wip_chem_eq_balancer.py
+++ wip_chem_eq_balancer.py
@@ -5,7 +5,7 @@ MgF2 + Li2CO3 = MgCO3 + LiF -> 1, 1, 1, 2
 Al + H2SO4 = Al2(SO4)3 + H3 -> 2, 3, 1, 3
 '''
 
-def splitter(eq):
+def lr_splitter(eq):
     ''' splits the equation into left and right '''
     d = {'left': [], 'right': []}
     eq = eq.split()
@@ -23,7 +23,25 @@ def splitter(eq):
 
     return d
 
+def atom_splitter(eq):
+    ''' splits the equation by molecule '''
+    d = {}
+    for i in range(len(eq)):
+        if eq[i].isalpha():
+            if eq[i].islower():
+                continue
+            elif i+1 < len(eq) and eq[i+1].islower():
+                d[eq[i] + eq[i+1]] = 0
+            else:
+                d[eq[i]] = 0
+
+    return d
+
 if __name__ == "__main__":
-    inp = input('enter equation: ')
-    parsed = splitter(inp)
+    # inp = input('enter equation: ')
+    inp = 'MgF2 + Li2CO3 = MgCO3 + LiF'
+    parsed = lr_splitter(inp)
     print(parsed)
+
+    moleculized = atom_splitter(inp)
+    print(moleculized)
