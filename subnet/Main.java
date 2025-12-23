e81efe3ba549751dbe45d05a9a8f458b71d6a3f4
KarinaJadia <karina.jadia@gmail.com>
2025-12-22 20:12:48 -0500

    :construction: wip

--- subnet/Main.java
+++ subnet/Main.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    
+
     System.out.println("enter subnet:");
     String response = input.nextLine();
     Subnet sub = new Subnet(response);
-    System.out.println(sub.raw);
+    System.out.println(sub.bionize());
 
     input.close();
   }
