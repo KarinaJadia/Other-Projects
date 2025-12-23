796fb4157dbe73378bd83ffeb15c05982f799ebc
KarinaJadia <karina.jadia@gmail.com>
2025-12-23 15:12:37 -0500

    :poop: i may be stupid but it's done

--- subnet.py
+++ subnet.py
@@ -17,18 +17,42 @@ def to_binary(b):
 
     return result
 
-if __name__ == "__main__":
-    # raw = input('input subnet: ') # 68.168.0.34/29
-    raw = '68.168.0.34/29'
+def sub_to_list(a):
+    ''' takes a subnet of format a.b.c.d/e and returns a list '''
+    b = a.split('.')
+    c = b[-1].split('/')
+    b.append(c[0])
+    b.append(c[1])
+    b.remove(b[-3])
 
-    # converting to array for ease of calculation
-    raw = raw.split('.')
-    c = raw[-1].split('/')
-    raw.append(c[0])
-    raw.append(c[1])
-    raw.remove(raw[-3])
+    return b
+
+def list_to_sub(b):
+    ''' takes a list and returns string in format a.b.c.d/e '''
+    return b[0] + '.' + b[1] + '.' + b[2] + '.' + b[3] + '/' + b[4]
+
+if __name__ == "__main__":
+    # r = input('input subnet: ') # 68.168.0.34/29
+    r = '68.168.0.34/29'
+
+    b = sub_to_list(r)
+
+    host_bits = 32 - int(b[-1])
     
-    for i in range(len(raw)):
-        raw[i] = to_binary(int(raw[i]))
+    for i in range(len(b)):
+        b[i] = to_binary(int(b[i]))
     
-    print(raw)
+    address = list_to_sub(b) # address in bin form
+    netmask = f'255.255.255.255'.split('.')
+    
+    # netmask is all ones except n zeros where n is number of host bits
+    for i in range(len(netmask)):
+        netmask[i] = to_binary(int(netmask[i]))
+
+    netmask[-1] = netmask[-1] = '1' * (8 - host_bits) + '0' * host_bits
+    netmask = netmask[0] + '.' + netmask[1] + '.' + netmask[2] + '.' + netmask[3]
+
+    print(f'original address: {r}')
+    print(f'binary address: {address}')
+    print(f'netmask: {netmask}')
+    print(f'hosts: {2**host_bits - 2}')
